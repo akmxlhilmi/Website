63f0e14dd1101a479fac7c109fce07d2b57f92b3
akmxlhilmi <trendsonic780@gmail.com>
2026-04-21 01:15:58 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// Use the Supabase public anon key here. Do not use the service_role key in the browser.
+window.CALENDAR_SUPABASE = {
+    url: "https://ysjsuvnmannskstoclrp.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzanN1dm5tYW5uc2tzdG9jbHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2OTAxMTcsImV4cCI6MjA5MjI2NjExN30.Oci8qNqUwHWdfG0CuaiuQScwaCeXs7UHGdzJ2sw3uDs"
+};
