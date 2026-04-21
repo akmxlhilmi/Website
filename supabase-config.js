d063e20c30dea978a97bd4348ffc9c767b7d3f99
akmxlhilmi <trendsonic780@gmail.com>
2026-04-22 01:51:29 +0800

    update comment

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-// Use the Supabase public anon key here. Do not use the service_role key in the browser.
+// Supabase public anon and URL
 window.CALENDAR_SUPABASE = {
     url: "https://ysjsuvnmannskstoclrp.supabase.co",
     anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzanN1dm5tYW5uc2tzdG9jbHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2OTAxMTcsImV4cCI6MjA5MjI2NjExN30.Oci8qNqUwHWdfG0CuaiuQScwaCeXs7UHGdzJ2sw3uDs"
